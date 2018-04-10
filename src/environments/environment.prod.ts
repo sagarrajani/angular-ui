fd45764d8d569b7bd786dc2b85876f33ebd08b1a
Sagar Rajani <sagarrajani@Sagars-MBP.home>
2018-04-09 21:54:50 -0400

    Deployment Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain: 'universalcreditservice.auth0.com',
+    clientID: 'DPG_iNtz9li8otKpNa5stjU33d1pibzw',
+    callbackOnLocationHash: true,
+    redirectOnSuccess: '/',
+  },
+
+  api: {
+    appList: 'http://private-87cbae-heavywatervodlistapplications.apiary-mock.com/rest/vodlistapplications',
+    addApp: 'http://private-1c675-heavywatervodorderservice.apiary-mock.com/rest/vodorder',
+    appDetails: 'http://private-0226e-heavywatervodfinalreport.apiary-mock.com/rest/vodreport',
+  },
 };
